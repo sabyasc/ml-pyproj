eef116153a667428005ec9b639d1d3790ea91782
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2024-12-22 23:56:01 +0530

    feat-01: update train.py and app.py with updated dataset nd functions

--- ml-pipelines/app.py
+++ ml-pipelines/app.py
@@ -4,18 +4,26 @@ github: https://github.com/sabyasc
 created: Dec 2024
 """
 from flask import Flask
-from models.train import data_ingestion
+from models.train import model_validation, model_testing
 
 app = Flask(__name__)
 
+# To check /api status
 @app.route("/api", methods = ['GET'])
 def status():
     return {'status': 'Success'}
 
+# To fetch /api/model details
 @app.route("/api/model", methods = ['GET'])
-def pre_process_api():
-    process_data = data_ingestion()
-    return process_data
+def data_processing_api():
+    processed_data = model_validation()
+    return processed_data
+
+# To fetch /api/model/test details
+@app.route("/api/model/test", methods = ['GET'])
+def data_testing_api():
+    test_data = model_testing()
+    return test_data
 
 if __name__ == '__main__':
     app.run(debug=True, port='5000')
