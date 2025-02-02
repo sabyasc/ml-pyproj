73c3d611ca03f7cb12eac5234a239f64e11d4a80
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-02 23:56:28 +0530

    fix-07: update reading file process with generic apis and csv upload path

--- src/preprocess/data_ingestion.py
+++ src/preprocess/data_ingestion.py
@@ -5,14 +5,25 @@ created: Dec 2024
 """
 import os
 import pandas as pd
+import requests
 
 # Data Ingestion is to fetech data from sources, We will follow below steps:
 # Step 1: Read data from source (csv, json, db, APIs, etc) using os and pandas,
 # Step 2: Display first 10 example rows of data to understand the data
 def ingestion():
     current_dir = os.path.dirname(os.path.abspath(__file__))
+    api_url = "https://jsonplaceholder.typicode.com/posts"
+    api_response = requests.get(api_url)
+    if api_response.ok:
+        api_data = api_response.json()
+        print("================ API Data Ingestion completed ================")
+    else:
+        api_data = {}
+        print("Failed to fetch data from API.")
+        
     data_path = os.path.join(os.path.dirname(os.path.dirname(current_dir)), 'data', 'raw', 'train.csv')
     data = pd.read_csv(data_path)
+    
     df = pd.DataFrame(data)
     count = df.head(50).to_dict()
     print("================ Data Ingestion completed ================")
