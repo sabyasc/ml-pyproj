bc6992c53ebfce64e104b302a77f41d8923d5e6d
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-02 23:36:36 +0530

    fix-07: fix bug of reading files with a generic way

--- src/preprocess/data_ingestion.py
+++ src/preprocess/data_ingestion.py
@@ -3,15 +3,17 @@ author: @sabyasc
 github: https://github.com/sabyasc/ml-pyproj
 created: Dec 2024
 """
+import os
 import pandas as pd
 
 # Data Ingestion is to fetech data from sources, We will follow below steps:
-# Step 1: Read data from source (csv, json, db, APIs, etc),
-# Step 2: Display first 10 example rows of data to understand the data,
-# Step 3: Return the data as Dataframe to next step for processing
+# Step 1: Read data from source (csv, json, db, APIs, etc) using os and pandas,
+# Step 2: Display first 10 example rows of data to understand the data
 def ingestion():
-    data = pd.read_csv(r'C:\GitHub\ml-pyproj\data\raw\train.csv')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(os.path.dirname(os.path.dirname(current_dir)), 'data', 'raw', 'train.csv')
+    data = pd.read_csv(data_path)
     df = pd.DataFrame(data)
-    count = df.head(50)
+    count = df.head(50).to_dict()
     print("================ Data Ingestion completed ================")
     return count
