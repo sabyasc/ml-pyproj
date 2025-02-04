5ee02b44b067b1cc3d7257e30e4d5a3a1863f0a0
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-04 23:40:32 +0530

    fix-07: update reading file process with generic apis and csv upload path

--- src/app.py
+++ src/app.py
@@ -3,34 +3,43 @@ author: @sabyasc
 github: https://github.com/sabyasc/ml-pyproj
 created: Jan 2025
 """
-from flask import Flask
-from model.model_training import train
-from model.model_tracking import model_tracking, model_testing
+from flask import Flask, redirect
+from preprocess.data_ingestion import ingestion
+# from model.model_training import train
+# from model.model_tracking import model_tracking, model_testing
 
 app = Flask(__name__)
+
+# Default endpoint which will redirect to /api
+@app.route('/', methods=['GET'])
+def default():
+    return redirect("/api")
 
 # To check /api status
 @app.route("/api", methods=['GET'])
 def status():
-    return {'status': 'Success'}
+    return {
+            'status': 'Success',
+            'message': 'Please use /api/model, /api/model/track, /api/model/test endpoints for your requests'
+            }
 
-# To fetch /model metadata
+# # To fetch /model metadata
 @app.route("/api/model", methods=['GET'])
 def model_metadata_api():
-    metadata = train()
+    metadata = ingestion()
     return metadata
 
-# To /track model performance
-@app.route("/api/model/track", methods=['GET'])
-def model_track_api():
-    metadata = model_tracking()
-    return metadata
+# # To /track model performance
+# @app.route("/api/model/track", methods=['GET'])
+# def model_track_api():
+#     metadata = model_tracking()
+#     return metadata
 
-# To /test model performance
-@app.route("/api/model/test", methods=['GET'])
-def model_test_api():
-    test_model = model_testing()
-    return test_model
+# # To /test model performance
+# @app.route("/api/model/test", methods=['GET'])
+# def model_test_api():
+#     test_model = model_testing()
+#     return test_model
 
 if __name__ == "__main__":
     app.run(port=5000)
