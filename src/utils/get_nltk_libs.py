73ade5663bd7cd743098ffa079bd444388c089fd
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-12 23:57:15 +0530

    feat-08: re design and optimize performance of flow by reducing nltk downloads

--- src/utils/get_nltk_libs.py
+++ src/utils/get_nltk_libs.py
@@ -1,7 +1,11 @@
-import nltk
-import os
+"""
+author: @sabyasc
+github: https://github.com/sabyasc/ml-pyproj
+created: Feb 2025
+"""
+import nltk, os
 
-# Dynamically determine the download directory relative to the project root
 nltk_data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "config", "nltk_libs"))
+
 nltk.download('punkt', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
