3f72d872e299a93c41bdf44bda8e05f208b2b455
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-17 23:54:39 +0530

    feat-08: update utils of nltk libs downloads

--- src/utils/get_nltk_libs.py
+++ src/utils/get_nltk_libs.py
@@ -5,7 +5,7 @@ created: Feb 2025
 """
 import nltk, os
 
+# Download NLTK libraries and store in config/nltk_libs
 nltk_data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "config", "nltk_libs"))
-
 nltk.download('punkt', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
