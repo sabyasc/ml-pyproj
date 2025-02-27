0537a1ce12af85db740a797edd851490769ba96f
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-02-27 23:53:19 +0530

    feat-08: update app.py for reading ingested data

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ created: Jan 2025
 """
 from flask import Flask, redirect
 from preprocess.data_ingestion import ingestion
+from preprocess.data_preprocessing import preprocessing
 # from model.model_training import train
 # from model.model_tracking import model_tracking, model_testing
 
@@ -26,7 +27,7 @@ def status():
 # # To fetch /model metadata
 @app.route("/api/model", methods=['GET'])
 def model_metadata_api():
-    metadata = ingestion()
+    metadata = preprocessing()
     return metadata
 
 # # To /track model performance
