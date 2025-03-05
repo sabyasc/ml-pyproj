9e2e8d669c189dd92b69c9d815b9bed48ce9669f
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-03-05 23:53:06 +0530

    feat-08: update data_preprocessing.py

--- src/preprocess/data_preprocessing.py
+++ src/preprocess/data_preprocessing.py
@@ -10,10 +10,6 @@ from textblob import TextBlob
 from data_ingestion import ingestion
 
 # Set NLTK's data path to the local nltk_libs folder in the config directory
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# local_nltk_path = os.path.join(os.path.dirname(os.path.dirname(current_dir)), 'config', 'nltk_libs')
-# local_nltk_path = os.path.join(os.getcwd(), 'config', 'nltk_libs')
-
 local_nltk_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "config", "nltk_libs"))
 if os.path.exists(local_nltk_path):
     if local_nltk_path not in nltk.data.path:
