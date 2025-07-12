4735ac6c90bf15a4a9029901841786a421443f72
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2025-07-12 23:37:00 +0530

    fix-8: update src/model/modelTracking.py

--- src/model/modelTracking.py
+++ src/model/modelTracking.py
@@ -2,7 +2,7 @@
 author: @sabyasc
 github: https://github.com/sabyasc/ml-pyproj
 created: Jan 2025
-updated: June 2025
+updated: July 2025
 """
 # src libs are fetched from __init__.py
 from src.model.modelEvaluation import validation 
